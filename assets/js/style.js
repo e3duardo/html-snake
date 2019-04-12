236188bf99cb682e097dfee5ec4076ef43b102a0
estagiario <otaviocha@gmail.com>
2019-04-12 16:52:23 -0300

    Done the logic to implement the scoreboard. The major problem was the massive use of arrow functions which may allow a quick start of the projects due to AF functionalities but at a long term will cause problems. Graphics improvements like background and centralization of the main board were also arranged.

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+var container = document.querySelector('.container');
+container.style.display = "flex";
+container.style.justifyContent = "center";
+
+var pannel = document.querySelector('.pannel');
+pannel.style.margin = "2vw";
+
+var github = document.querySelector('.github');
+github.style.display = "block";
+github.style.margin = "0 auto";
+github.style.height = "1.5vw";
